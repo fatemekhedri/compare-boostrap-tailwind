272651cf82963eb9df8d77891d3bef3524e2f4ba
fatemeh Khedri <khedri.fateme92@gmail.com>
2022-02-13 16:42:55 +0330

    edit

--- src/flexGallery.jsx
+++ src/flexGallery.jsx
@@ -5,20 +5,20 @@ function FlexGallery() {
     <div>
       <h1>Flex Gallery</h1>
       <div className="d-flex">
-        <div className="m-3 d-flex justify-content-center p-5">
-          <span className="rounded bg-danger p-4">1</span>
+        <div className="m-3 d-flex justify-content-center bg-warning align-items-center box-grid-size">
+          <span className="rounded bg-danger p-2">1</span>
         </div>
-        <div className="m-3 d-flex justify-content-center p-5">
-          <span className="rounded bg-danger p-4">2</span>
+        <div className="m-3 d-flex justify-content-center bg-warning align-items-center box-grid-size">
+          <span className="rounded bg-danger p-2">2</span>
         </div>
-        <div className="m-3 d-flex justify-content-center p-5">
-          <span className="rounded bg-danger p-4">3</span>
+        <div className="m-3 d-flex justify-content-center bg-warning align-items-center box-grid-size">
+          <span className="rounded bg-danger p-2">3</span>
         </div>
-        <div className="m-3 d-flex justify-content-center p-5">
-          <span className="rounded bg-danger p-4">4</span>
+        <div className="m-3 d-flex justify-content-center bg-warning align-items-center box-grid-size">
+          <span className="rounded bg-danger p-2">4</span>
         </div>
-        <div className="m-3 d-flex justify-content-center p-5">
-          <span className="rounded bg-danger p-4">5</span>
+        <div className="m-3 d-flex justify-content-center bg-warning align-items-center box-grid-size ">
+          <span className="rounded bg-danger p-2">5</span>
         </div>
       </div>
     </div>
