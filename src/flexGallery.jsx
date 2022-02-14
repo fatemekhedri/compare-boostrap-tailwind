8fa739441eab733a8ea6ff0fda89988e60202ae4
fatemeh Khedri <khedri.fateme92@gmail.com>
2022-02-14 09:28:27 +0330

    use tailwind

--- src/flexGallery.jsx
+++ src/flexGallery.jsx
@@ -5,16 +5,16 @@ function FlexGallery() {
     <div>
       <h1>Flex Gallery</h1>
       <div className="flex">
-        <div className="m-3 flex bg-emerald-100 justify-center items-center box-size-gallery ">
-          <span className="rounded bg-emerald-500 p-4">1</span>
+        <div className="m-2 flex bg-emerald-100 justify-center items-center box-size-gallery ">
+          <span className="rounded bg-emerald-500 p-2">1</span>
+        </div>
+        <div className="m-1 flex bg-emerald-100 justify-center items-center box-size-gallery">
+          <span className="rounded btn bg-emerald-500 p-1">2</span>
         </div>
         <div className="m-3 flex bg-emerald-100 justify-center items-center box-size-gallery">
-          <span className="rounded bg-emerald-500 p-4">2</span>
+          <span className="rounded bg-emerald-500 p-3">3</span>
         </div>
-        <div className="m-3 flex bg-emerald-100 justify-center items-center box-size-gallery">
-          <span className="rounded bg-emerald-500 p-4">3</span>
-        </div>
-        <div className="m-3 flex bg-emerald-100 justify-center items-center box-size-gallery">
+        <div className="m-4 flex bg-emerald-100 justify-center items-center box-size-gallery">
           <span className="rounded bg-emerald-500 p-4">4</span>
         </div>
         <div className="m-3 flex bg-emerald-100 justify-center items-center box-size-gallery">
--- src/gridGallery.jsx
+++ src/gridGallery.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 
 function GridGallery() {
   return (
+      <>
     <div>
       <h1>Gallery grid</h1>
       <div className="flex">
@@ -17,11 +18,48 @@ function GridGallery() {
         <div className="w-32 bg-rose-300 h-32">
           <span className="rounded bg-yellow-300 p-4">4</span>
         </div>
-        {/* <div className="m-3  w-32 h-32">
-          <span className="rounded bg-yellow-300 p-4">5</span>
-        </div> */}
+      </div>
+      <div className="flex flex-row mt-3">
+        <div className="  basis-1/4 bg-rose-300 h-32">
+          <span className="rounded bg-yellow-300 p-4">1</span>
+        </div>
+        <div className="mx-4 basis-1/4 bg-rose-300 h-32">
+          <span className="rounded bg-yellow-300 p-4">2</span>
+        </div>
+        <div className="  basis-1/6 bg-rose-300 h-32">
+          <span className="rounded bg-yellow-300 p-4">3</span>
+        </div>
       </div>
     </div>
+    <div>
+      <h1>Gallery grid</h1>
+      <div className="grid grid-flow-row-dense grid-cols-3 grid-rows-3">
+        <div className="gap-4 grid bg-rose-300 grid-cols-2">
+          <span className="rounded bg-yellow-300 text-4xl p-4">1</span>
+        </div>
+        <div className="gap-4 grid bg-rose-300 grid-cols-2 ">
+          <span className="rounded bg-yellow-300 p-4">2</span>
+        </div>
+        <div className="gap-4 grid bg-rose-300 grid-cols-2">
+          <span className="rounded bg-yellow-300 p-4">3</span>
+        </div>
+        <div className="gap-4 grid bg-rose-300 grid-cols-2">
+          <span className="rounded bg-yellow-300 p-4">4</span>
+        </div>
+      </div>
+      <div className="flex flex-row mt-3">
+        <div className="  basis-1/4 bg-rose-300 h-32">
+          <span className="rounded bg-yellow-300 p-4">1</span>
+        </div>
+        <div className="mx-4 basis-1/4 bg-rose-300 h-32">
+          <span className="rounded bg-yellow-300 p-4">2</span>
+        </div>
+        <div className="  basis-1/6 bg-rose-300 h-32">
+          <span className="rounded bg-yellow-300 p-4">3</span>
+        </div>
+      </div>
+    </div>
+    </>
   );
 }
 
